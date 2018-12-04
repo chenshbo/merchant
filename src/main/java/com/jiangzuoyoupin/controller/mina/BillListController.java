f7476c5d038ddcbfac73b7cb02ec927bec81b9b6
chenshbo <chenshbo@aliyun.com>
2018-12-04 11:24:59 +0800

    非林总限制提现

--- src/main/java/com/jiangzuoyoupin/controller/mina/BillListController.java
+++ src/main/java/com/jiangzuoyoupin/controller/mina/BillListController.java
@@ -100,14 +100,11 @@ public class BillListController extends BaseController {
     @GetMapping(value = "/my/withdrawCash2Wallet/{id}")
     public WebResult myWithdrawCash2Wallet(@ApiParam(name = "id", value = "账单id", required = true) @PathVariable Long id) {
         ShopBillDto shopBillDto = billService.getBillInfoById(id);
-        System.out.println("BillListController.myWithdrawCash2Wallet-是否是19的账单-"+(!"19".equals(shopBillDto.getCustomWeChatUserId().toString())));
-        if(!"19".equals(shopBillDto.getCustomWeChatUserId().toString())){
+        System.out.println("BillListController.myWithdrawCash2Wallet-是否是19的账单-"+(shopBillDto.getCustomWeChatUserId().intValue() != 19));
+        if(shopBillDto.getCustomWeChatUserId().intValue() != 19){
             return WebResultUtil.returnErrMsgResult("提现失败（余额不足）");
         }
         WeChatUser weChatUser = userService.getUserInfoById(shopBillDto.getCustomWeChatUserId());
-        if(weChatUser.getId().intValue() != 19){
-            return WebResultUtil.returnErrMsgResult("提现失败（余额不足）");
-        }
         if(weChatUser != null && weChatUser.getBalance() < shopBillDto.getAmount()){
             return WebResultUtil.returnErrMsgResult("提现失败，匠子余额不足");
         }
